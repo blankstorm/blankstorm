6080559bc648cdcaab85bed171027bb7295f099b
Vortex 418 <prevett.james@gmail.com>
2022-10-06 06:19:09 -0500

    Formatting

--- core.js
+++ core.js
@@ -56,33 +56,33 @@ const greek = [
 		'Psi',
 		'Omega',
 	],
-	numeral(number){
-let num = number+0 ?? 0,
-  roman = [
-    [M, 1000],
-    [CM, 900],
-    [D, 500],
-    [CD, 400],
-    [C, 100],
-    [XC, 90],
-    [L, 50],
-    [XL, 40],
-    [X, 10],
-    [IX, 9],
-    [V, 5],
-    [IV, 4],
-    [I, 1]
-  ],
-  result = '';
+	numeral = (number) => {
+		let num = number+0 ?? 0,
+		roman = [
+			[M, 1000],
+			[CM, 900],
+			[D, 500],
+			[CD, 400],
+			[C, 100],
+			[XC, 90],
+			[L, 50],
+			[XL, 40],
+			[X, 10],
+			[IX, 9],
+			[V, 5],
+			[IV, 4],
+			[I, 1]
+		],
+		result = '';
 
-  for (let [key, val] of roman) {
-    let q = Math.floor(num / val);
-    num -= q * val;
-    result += key.repeat(q);
-  }
+		for (let [key, val] of roman) {
+			let q = Math.floor(num / val);
+			num -= q * val;
+			result += key.repeat(q);
+		}
 
-  return result;
-}
+		return result;
+	}
 	minimize = Intl.NumberFormat('en', { notation: 'compact' }).format,
 	range = (min, max) => {
 		let a = [];
