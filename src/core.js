ca8d31751c61bea0c55f222dbba2819e89d9fd1d
Vortex 418 <prevett.james@gmail.com>
2022-12-27 04:10:32 -0600

    Added I connecter and worked on station mechanics

--- src/core.js
+++ src/core.js
@@ -1248,6 +1248,7 @@ const StationComponent = class extends BABYLON.TransformNode {
 	#station;
 	#resolve;
 	instanceReady;
+	connections = [];
 	constructor(type, station, id = random.hex(32)) {
 		if (!(station instanceof Station)) throw new TypeError();
 		if (!(station.level instanceof Level)) throw new TypeError();
@@ -1256,7 +1257,6 @@ const StationComponent = class extends BABYLON.TransformNode {
 		this._generic = StationComponent.generic.get(type);
 
 		this.type = type;
-		this.parent = station;
 		this.#station = station;
 
 		let resolve;
@@ -1274,15 +1274,57 @@ const StationComponent = class extends BABYLON.TransformNode {
 	}
 
 	async #createInstance() {
-		this.mesh = await this.level.instantiateGenericMesh(this.type);
+		this.mesh = await this.level.instantiateGenericMesh('station.' + this.type);
 		this.mesh.setParent(this);
 		this.mesh.position = BABYLON.Vector3.Zero();
 		this.mesh.rotation = new BABYLON.Vector3(0, 0, Math.PI);
 		this.#resolve();
 	}
 
+	addConnection(component, connecter, componentConnecter) {
+		let connection1 = this._generic.connecters.at(connecter),
+			connection2 = component._generic.connecters.at(componentConnecter);
+		if (!connection1) {
+			throw new ReferenceError(`Connecter "${connecter}" does not exist`);
+		}
+		if (!connection2) {
+			throw new ReferenceError(`Subcomponent connecter "${componentConnecter}" does not exist`);
+		}
+
+		this.#station.components.push(component);
+		this.connections[connecter] = component;
+		component.connections[componentConnecter] = this;
+
+		component.parent = this;
+		component.position = connection1.position.clone()//.add(connection2.position);
+		component.rotation = connection1.rotation.clone()//.add(connection2.rotation);
+	}
+
+	removeConnection(component) {
+		this.connections[this.connections.indexOf(component)] = undefined;
+		component.connections[component.connections.indexOf(this)] = undefined;
+	}
+
+	removeConnectionAtIndex(connecter) {
+		const component = this.connections[connecter];
+		return this.removeConnection(component);
+	}
+
+	serialize() {
+		return {
+			id: this.id,
+			type: this.type,
+			position: this.position.asArray(),
+			rotation: this.rotation.asArray(),
+			connections: this.connections.map(component => component.serialize())
+		};
+	}
+
 	remove() {
 		this.#station.components.splice(this.#station.components.indexOf(this), 1);
+		for(let connection of this.connections) {
+			this.removeConnection(connection);
+		}
 		this.dispose();
 	}
 
@@ -1293,7 +1335,24 @@ const StationComponent = class extends BABYLON.TransformNode {
 				type: 'core',
 				hp: 100,
 				model: 'models/station/core.glb',
-				connecters: [{ type: 'any', position: new BABYLON.Vector3(0, 0, 0) }],
+				connecters: [
+					{ type: '*', position: new BABYLON.Vector3(0, 0, 0), rotation: new BABYLON.Vector3(0, 0, 0) },
+					{ type: '*', position: new BABYLON.Vector3(0, 0, 0), rotation: new BABYLON.Vector3(0, Math.PI / 2, 0) },
+					{ type: '*', position: new BABYLON.Vector3(0, 0, 0), rotation: new BABYLON.Vector3(0, Math.PI, 0) },
+					{ type: '*', position: new BABYLON.Vector3(0, 0, 0), rotation: new BABYLON.Vector3(0, (3 * Math.PI) / 2, 0) },
+				],
+			},
+		],
+		[
+			'connecter_i',
+			{
+				type: 'connecter',
+				hp: 50,
+				model: 'models/station/connecter_i.glb',
+				connecters: [
+					{ type: '*', position: new BABYLON.Vector3(0, 0, -0.5), rotation: BABYLON.Vector3.Zero() },
+					{ type: '*', position: new BABYLON.Vector3(0, 0, 0.5), rotation: BABYLON.Vector3.Zero() },
+				],
 			},
 		],
 	]);
@@ -1301,16 +1360,34 @@ const StationComponent = class extends BABYLON.TransformNode {
 
 const Station = class extends CelestialBody {
 	components = [];
+	#core;
 	#level;
 
 	constructor({ name = 'Station', id = random.hex(32) }, level) {
 		super(name, id, level);
 
 		this.#level = level;
+		this.#core = new StationComponent('core', this);
+		this.#core.parent = this;
 	}
 
 	get level() {
 		return this.#level;
+	}
+
+	get core() {
+		return this.#core;
+	}
+
+	serialize() {
+		return {
+			id: this.id,
+			components: this.#core.serialize()
+		};
+	}
+
+	remove() {
+
 	}
 };
 
