2f9a0a62856bdd7a5add7a4755cee700d2841a05
Dr. Vortex <jp@drvortex.dev>
2023-08-28 14:55:16 -0500

    Changed asset system

---
+++ assets/models/export.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:a68d0e1f3d8eb7471d694840cef33166fd265caaf757bc258b54c135d2a32405
+size 479
---
+++ assets/models/export.sh
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:f0b349f01fd2b3bc21386aafd9b2e579e14170d6a9ae85e8a006e2b55cf83536
+size 421
