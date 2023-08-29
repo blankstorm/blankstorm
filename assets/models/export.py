a6822ce86af5c2e400214f9936904e5a0ca94ffd
Dr. Vortex <prevett.james@gmail.com>
2023-08-29 06:56:37 -0500

    Fixed export script

--- assets/models/export.py
+++ assets/models/export.py
@@ -5,11 +5,18 @@ dirname = os.path.dirname(__file__)
 cwd = os.getcwd()
 os.chdir(dirname)
 for filename in os.listdir(dirname):
-	if not filename.endswith('.blend'):
+	try:
+		if not filename.endswith('.blend'):
+			continue
+		filename = os.path.splitext(filename)[0]
+		path = os.path.join(dirname, filename)
+		print('Exporting: ' + filename)
+		bpy.ops.wm.open_mainfile(filepath=path + '.blend')
+		with bpy.context.copy():
+			bpy.context.active_object = None
+			bpy.ops.export_scene.gltf(filepath=path + '.glb', export_copyright='Dr. Vortex', export_apply=True)
+	except Exception as e:
+		print(e)
 		continue
-	filename = os.path.basename(filename)
-	path = os.path.join(dirname, filename) + 'glb'
-	print('Exporting: ' + filename)
-	bpy.ops.wm.open_mainfile(filepath=path)
-	bpy.ops.export_scene.gltf(filepath=path, export_copyright='Dr. Vortex', export_apply=True)
 os.chdir(cwd)
+bpy.ops.wm.quit_blender()
--- assets/models/export.sh
+++ assets/models/export.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
-parent_path=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P ) # https://stackoverflow.com/a/24112741
+dir_path=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P ) # https://stackoverflow.com/a/24112741
+
 
 # Check if Blender is not installed
 if ! command -v blender >/dev/null 2>&1; then
@@ -9,5 +10,5 @@ fi
 
 # Blender is installed, so run Blender with Python script
 echo "found Blender! Exporting models..."
-blender -P export.py
+blender -P "$dir_path/export.py"
 echo "Done!"
