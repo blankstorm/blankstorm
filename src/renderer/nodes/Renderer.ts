5153f6e5c5f78c3439e944b8ba015bd643c0a785
Dr. Vortex <jp@drvortex.dev>
2023-09-07 14:53:14 -0500

    Changed Renderer to use declare class

--- src/renderer/nodes/Renderer.ts
+++ src/renderer/nodes/Renderer.ts
@@ -2,10 +2,7 @@ import type { TransformNode } from '@babylonjs/core/Meshes/transformNode';
 import type { Scene } from '@babylonjs/core/scene';
 import type { SerializedNode } from '../../core/nodes/Node';
 
-export interface Renderer<S extends SerializedNode> extends TransformNode {
-	update(s: S): Promise<unknown>;
-}
-
-export interface RendererStatic<S extends SerializedNode> {
-	FromJSON(s: S, scene: Scene): Promise<Renderer<S>>;
+export declare class Renderer<S extends SerializedNode> extends TransformNode {
+	update(s: S): Promise<void>;
+	static FromJSON<S extends SerializedNode>(s: S, scene: Scene): Promise<Renderer<S>>;
 }
