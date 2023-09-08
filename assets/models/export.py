1aaa4df788f285f46e0ca3493c82abd66bda7eab
Dr. Vortex <jp@drvortex.dev>
2023-09-08 14:55:06 -0500

    Updated model scripts Applied model transformations Changed ship texture to png

--- assets/models/export.py
+++ assets/models/export.py
@@ -1,20 +1,29 @@
 import os
+import argparse
 import bpy
 
 dirname = os.path.dirname(__file__)
+
+parser = argparse.ArgumentParser()
+parser.add_argument('-i', '--input', help='input directory', default=dirname)
+parser.add_argument('-o', '--output', help='output directory', default=dirname)
+parser.add_argument('-v', '--verbose', help='verbose logs', action='store_true')
+args = parser.parse_args()
+
 cwd = os.getcwd()
-os.chdir(dirname)
-for filename in os.listdir(dirname):
+os.chdir(args.input)
+for filename in os.listdir(args.input):
 	try:
 		if not filename.endswith('.blend'):
 			continue
 		filename = os.path.splitext(filename)[0]
-		path = os.path.join(dirname, filename)
+		blend_path = os.path.join(args.input, filename) + '.blend'
+		export_path = os.path.join(args.output, filename) + '.glb'
 		print('Exporting: ' + filename)
-		bpy.ops.wm.open_mainfile(filepath=path + '.blend')
+		bpy.ops.wm.open_mainfile(filepath=blend_path)
 		with bpy.context.copy():
 			bpy.context.active_object = None
-			bpy.ops.export_scene.gltf(filepath=path + '.glb', export_copyright='Dr. Vortex', export_apply=True)
+			bpy.ops.export_scene.gltf(filepath=export_path, export_copyright='Dr. Vortex', export_apply=True)
 	except Exception as e:
 		print(e)
 		continue
