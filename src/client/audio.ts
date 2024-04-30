140a65ad843df8f31e2c24a081d799e473172932
James P <jp@drvortex.dev>
2024-04-30 08:11:29 -0500

    Fixed and reoganized sounds

--- src/client/audio.ts
+++ src/client/audio.ts
@@ -1,18 +1,6 @@
-export const sounds = new Map(
-	Object.entries({
-		rift: 'music/rift',
-		planets: 'music/planets',
-		destroy_ship: 'sfx/destroy_ship',
-		warp_start: 'sfx/warp_start',
-		warp_end: 'sfx/warp_end',
-		laser_fire: 'sfx/laser_fire',
-		laser_hit: 'sfx/laser_hit',
-	})
-);
-
 export function playsound(id: string, vol = 1) {
 	if (vol > 0) {
-		const a = new Audio(`${$build.asset_dir}/${sounds.get(id)}.mp3`);
+		const a = new Audio(`${$build.asset_dir}/sounds/${id}.mp3`);
 		a.volume = +vol;
 		a.play();
 	}
