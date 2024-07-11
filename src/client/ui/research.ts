8a4b061254df6a7fd4cf89df1a39c207d6b48d2a
James P. <jp@drvortex.dev>
2024-07-10 21:15:48 -0500

    Formatting

--- src/client/ui/research.ts
+++ src/client/ui/research.ts
@@ -8,8 +8,8 @@ export function createResearchUI(research: Research): DocumentFragment {
 	const instance = instaniateTemplate('#research');
 	instance.querySelector('.name')!.textContent = locales.text(`tech.${research.id}.name`);
 	instance.querySelector('.upgrade')!.addEventListener('click', () => action('research', research));
-	const $lab = $('div.lab')
-	console.log($lab[0])
+	const $lab = $('div.lab');
+	console.log($lab[0]);
 	$lab.append(instance);
 	return instance;
 }
