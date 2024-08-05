498864efd6ca6eb1ac202892c5bb60c80e55d515
James Prevett <jp@jamespre.dev>
2024-08-05 14:27:54 -0500

    Added postject typings

---
+++ scripts/postject.d.ts
@@ -0,0 +1,20 @@
+// Postject does not define types
+
+declare module 'postject' {
+	interface InjectOptions {
+		/**
+		 * @default '__POSTJECT'
+		 */
+		machoSegmentName?: string;
+		/**
+		 * @default false
+		 */
+		overwrite?: boolean;
+		/**
+		 * @default "POSTJECT_SENTINEL_fce680ab2cc467b6e072b8b5df1996b2"
+		 */
+		sentinelFuse?: string;
+	}
+
+	function inject(filename: string, resourceName: string, resourceData: Buffer, options: InjectOptions);
+}
